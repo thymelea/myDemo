56c9c0f3f49818ae362e0b30b2d3f8dac9600627
thymelea <34881613+thymelea@users.noreply.github.com>
2018-02-28 13:45:30 +0800

    用户

--- src/main/java/winter/SpringbootMybatisDemoApplication.java
+++ src/main/java/winter/SpringbootMybatisDemoApplication.java
@@ -3,10 +3,15 @@ package winter;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import winter.config.StartupListener;
+import winter.config.SysConfig;
 
 @SpringBootApplication
+@EnableWebSecurity
+@EnableConfigurationProperties({SysConfig.class})
 @MapperScan("winter.mapper")
 @EnableScheduling
 public class SpringbootMybatisDemoApplication {
