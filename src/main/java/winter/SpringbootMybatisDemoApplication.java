a1e033960f7dcedcde777c71fd300887c80ff721
thymelea <34881613+thymelea@users.noreply.github.com>
2018-02-28 10:16:59 +0800

    触发

--- src/main/java/winter/SpringbootMybatisDemoApplication.java
+++ src/main/java/winter/SpringbootMybatisDemoApplication.java
@@ -3,12 +3,17 @@ package winter;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import winter.config.StartupListener;
 
 @SpringBootApplication
 @MapperScan("winter.mapper")
+@EnableScheduling
 public class SpringbootMybatisDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootMybatisDemoApplication.class, args);
+		SpringApplication springApp=new SpringApplication(SpringbootMybatisDemoApplication.class);
+		springApp.addListeners(new StartupListener());
+		springApp.run(args);
 	}
 }
--- src/main/java/winter/schedule/scheduleTest.java
+++ src/main/java/winter/schedule/scheduleTest.java
@@ -1,11 +1,17 @@
 package winter.schedule;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
+@Component
 public class scheduleTest {
+    private final static Logger logger = LoggerFactory.getLogger(scheduleTest.class);
 
-    @Scheduled(cron = "0 0 2 * * ?")
+    @Scheduled(cron = "0 * * * * ?")
     public void test(){
         System.out.println("aaa");
+        logger.error("rrrrrrrrrrrrrr");
     }
 }
